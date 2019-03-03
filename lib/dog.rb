7c83d3734b37fc12af495309d78a5e82b3a36728
Jenny Chu <jchudesign0930@gmail.com>
2019-03-03 00:36:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,18 @@ class Dog
   
   def name 
     @name
-end
+  end
 
-def name= (new_name)
+  def name= (new_name)
  @name = new_name
-end
+  end
+
+  def breed
+  @breed
+  end
+  
+  def breed= (new_breed)
+    @breed = new_breed
+  end
+  
 end
